abd76d79df7b4d32aa2b4d518f57ff3ceedc5e7b
Timur Malyshev <wqs-info@mail.ru>
2018-06-15 19:11:46 +0500

    add default/specified method in routing

--- core/Router.php
+++ core/Router.php
@@ -16,9 +16,14 @@ class Router
             $route = $routes[$key];
 
             $class_name = $route['class'];
-            $method = $route['method'];
+
+            $method = $route['method']; //default method from config
 
             $class = new $class_name();
+
+            if(isset(Input::$path_segments[1]) and method_exists($class_name,Input::$path_segments[1])){ // if method was specified in url
+                $method = Input::$path_segments[1];
+            }
 
             $class->$method();
 
